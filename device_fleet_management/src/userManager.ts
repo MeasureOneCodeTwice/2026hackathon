9120a1c29e42dfd8b6e96adf246e0e8df4797b6b
ilikeRocks0 <125428652+ilikeRocks0@users.noreply.github.com>
2026-03-14 11:08:08 -0500

    Passed User test

--- device_fleet_management/src/userManager.ts
+++ device_fleet_management/src/userManager.ts
@@ -7,30 +7,64 @@ export interface User {
 }
 
 export class UserManager {
+    users: Map<string, User> = new Map();
 
     addUser(user: User): void {
+      if(user.id.length === 0){
+        throw new Error("User must have an id");
+      }
+      if(this.users.has(user.id)){
+        throw new Error(`User with id ${user.id} already exists`)
+      }
+
+      this.users.set(user.id, user);
     }
 
     removeUser(id: string): void {
+      if(!this.users.has(id)){
+        throw new Error(`User with id ${id} not found`)
+      }
+
+      this.users.delete(id);
     }
 
     getUser(id: string): User | null {
+      if (this.users.has(id)){
+        let user: User | undefined = this.users.get(id);
+        if (user === undefined){
+          return null;
+        }
+        return user;
+      }
+      
       return null;
     }
 
     getUsersByEmail(email: string): User[] | null {
-      return null;
+      let userEmail:User[] = []
+      this.users.forEach((value, key) =>{
+        if(value.email === email){
+          userEmail.push(value)
+        }
+      })
+      return userEmail;
     }
 
     getUsersByPhone(phone: string): User[] | null {
-      return null;
+      let userPhone:User[] = []
+      this.users.forEach((value, key) =>{
+        if(value.phone === phone){
+          userPhone.push(value)
+        }
+      })
+      return userPhone;
     }
 
     getAllUsers(): User[] {
-        return [];
+        return Array.from(this.users.values());
     }
 
     getUserCount(): number {
-        return 0;
+        return this.users.size;
     }
 }
